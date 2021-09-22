19df3fba9dc5c951fddc4e61d94f8a8177ee9a69
chinxzy <chinxzypoet@gmail.com>
2021-09-22 01:28:42 +0100

    fix

--- converter.js
+++ converter.js
@@ -1,10 +1,10 @@
 function meter(Val) {
-    document.getElementById("kilometers").value=Val*1000;
+    document.getElementById("kilometers").value=Val/1000;
     document.getElementById("feet").value=Val*3.28;
 }
 
 function kilometer(Val) {
-    document.getElementById("meters").value=Val/1000;
+    document.getElementById("meters").value=Val*1000;
     document.getElementById("feet").value=Val*3281;
 }
 
