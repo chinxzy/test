b34189b719c5a8a0cb7ed43ea4459d96fff82f2d
chinxzy <chinxzypoet@gmail.com>
2021-09-22 01:16:21 +0100

    lenght converter

---
+++ converter.js
@@ -0,0 +1,14 @@
+function meter(Val) {
+    document.getElementById("kilometers").value=Val*1000;
+    document.getElementById("feet").value=Val*3.28;
+}
+
+function kilometer(Val) {
+    document.getElementById("meters").value=Val/1000;
+    document.getElementById("feet").value=Val*3281;
+}
+
+function feet(Val) {
+    document.getElementById("meters").value=Val*0.30;
+    document.getElementById("kilometers").value=Val*0.00030;
+}
